26b746ab3867e5cf70b7a243dd79b1edb3732949
Jonas Oliveira <jonasfissicaro13@gmail.com>
2019-12-28 15:07:28 -0300

    fix api dto

--- Dtos/Movement.cs
+++ Dtos/Movement.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace jogoVelha.Dtos
 {
     public class Movement
     {
+        [Required]
         public string Player { get; set; }
+        [Required]
         public Position Position { get; set; }
     }
 }
